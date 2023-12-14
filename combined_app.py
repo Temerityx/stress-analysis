29d55541d74853755cbe37fa31e11958eaa200ee
Vignesh <91306520+Temerityx@users.noreply.github.com>
2023-12-15 00:44:57 +0530

    Add files via upload

---
+++ combined_app.py
@@ -0,0 +1,85 @@
+# combined_app.py
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load the data
+df = pd.read_csv("merged.csv", index_col=0)
+
+# Define labels
+labels = {0: "Amused", 1: "Neutral", 2: "Stressed"}
+
+# Define selected features
+selected_feats = [
+    "BVP_mean", "BVP_std", "EDA_phasic_mean", "EDA_phasic_min", "EDA_smna_min",
+    "EDA_tonic_mean", "Resp_mean", "TEMP_mean", "TEMP_std", "TEMP_slope",
+    "BVP_peak_freq", "age", "height", "weight",
+]
+
+# Prepare features and target variable
+X = df[selected_feats]
+y = df["label"]
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+
+# Train the Random Forest model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# creating a function for Prediction
+def stress_detection(input_data):
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+
+    prediction = model.predict(input_data_reshaped)
+
+    if prediction[0] == 0:
+        return 'The person is amused'
+    elif prediction[0] == 1:
+        return 'The person is neutral'
+    else:
+        return 'The person is stressed'
+
+
+def main():
+    # giving a title
+    st.title('Stress Detection Web App')
+
+    # getting the input data from the user
+    BVP_mean = st.text_input('BVP_mean')
+    BVP_std = st.text_input('BVP_std')
+    EDA_phasic_mean = st.text_input('EDA_phasic_mean')
+    EDA_phasic_min = st.text_input('EDA_phasic_min')
+    EDA_smna_min = st.text_input('EDA_smna_min')
+    EDA_tonic_mean = st.text_input('EDA_tonic_mean')
+    Resp_mean = st.text_input('Resp_mean')
+    TEMP_mean = st.text_input('TEMP_mean')
+    TEMP_std = st.text_input('TEMP_std')
+    TEMP_slope = st.text_input('TEMP_slope')
+    BVP_peak_freq = st.text_input('BVP_peak_freq')
+    age = st.number_input("Age")
+    height = st.number_input("Height")
+    weight = st.number_input("Weight")
+
+    # code for Prediction
+    pred = ''
+
+    # creating a button for Prediction
+    if st.button('Diabetes Test Result'):
+        pred = stress_detection([BVP_mean, BVP_std, EDA_phasic_mean, EDA_phasic_min, EDA_smna_min,
+                                 EDA_tonic_mean, Resp_mean, TEMP_mean, TEMP_std, TEMP_slope,
+                                 BVP_peak_freq, age, height, weight])
+
+    st.success(pred)
+
+
+if __name__ == '__main__':
+    main()
